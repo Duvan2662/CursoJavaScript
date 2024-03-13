487b39cc788fd2e24e1e7edefc6c12587ab23ba4
Duvan <duvanriano2626@gmail.com>
2024-03-13 07:17:38 -0500

    optimizaciones al juego parte 1

--- 02-juego-blackjack/assets/js/juego.js
+++ 02-juego-blackjack/assets/js/juego.js
@@ -5,207 +5,210 @@
 * 2S = DOS DE ESPADAS 
 */
 
-let baraja            = [];
-let puntosJugador     = 0;
-let puntosComputadora = 0 ;
 
-const tipos      = ['C','D','H','S']; //Tipos de carta
-const especiales = ['A','J','Q','K']; //Tipos de carta
+( () =>{
+    'use strict'
 
-//Referencias del DOM
-const btnpedir          = document.querySelector('#btnPedir');
-const btndetener        = document.querySelector('#btnDetener');
-const btnuevo           = document.querySelector('#btnNuevo');
-const actualizarPuntos  = document.querySelectorAll('small');
-const barajaJugador     = document.querySelector('#jugador-cartas');
-const barajaComputadora = document.querySelector('#computadora-cartas');
+    let baraja            = [],
+        puntosJugadores   = [];
 
 
+    const tipos      = ['C','D','H','S'], //Tipos de carta
+          especiales = ['A','J','Q','K']; //Tipos de carta
 
-
-
-//Esta funcion crea una nueva baraja 
-const crearBaraja = () =>{
+    //Referencias del DOM
+    const btnpedir          = document.querySelector('#btnPedir'),
+          btndetener        = document.querySelector('#btnDetener'),
+          btnuevo           = document.querySelector('#btnNuevo'),
+          actualizarPuntos  = document.querySelectorAll('small'),
+          barajaJugador     = document.querySelector('#jugador-cartas'),
+          barajaComputadora = document.querySelector('#computadora-cartas');
     
-    //Llena el arreglo de baraja con los nueros del 2 al 10 y los diferentes tipos de carta(c,d,h,s) ejemlo: (2C,3D,4S)
-    for (let i = 2; i<= 10; i++) {
-       for (let tipo of  tipos) {
-        baraja.push(i+tipo)
-       }
-    }
-    //Llena el arreglo de baraja con las cartas especiales (a,j,q,k) y los diferentes tipos de carta(c,d,h,s) (AC,JC,QCKC)
-    for (let tipo of tipos) {
-        for (let esp of especiales) {
-            baraja.push(esp+tipo)
+
+
+
+    //Esta funcion inicialia el juego      
+    const crearJuego = (numJugadores = 2)=> {
+        baraja = crearBaraja();
+        for (let i = 0; i < numJugadores; i++) {
+            puntosJugadores.push(0);
         }
+        console.log({puntosJugadores});
+    };
+
+
+
+    //Esta funcion crea una nueva baraja 
+    const crearBaraja = () =>{
+        baraja = [];
+        //Llena el arreglo de baraja con los nueros del 2 al 10 y los diferentes tipos de carta(c,d,h,s) ejemlo: (2C,3D,4S)
+        for (let i = 2; i<= 10; i++) {
+        for (let tipo of  tipos) {
+            baraja.push(i+tipo)
+        }
+        }
+
+        //Llena el arreglo de baraja con las cartas especiales (a,j,q,k) y los diferentes tipos de carta(c,d,h,s) (AC,JC,QCKC)
+        for (let tipo of tipos) {
+            for (let esp of especiales) {
+                baraja.push(esp+tipo)
+            }
+        }
+
+        return _.shuffle (baraja);//Funcion que sirve para barajar apartir de la libreria Underscore.js
+    };
+
+    //Esta funcion me permite tomar una nueva carta 
+    const tomarCarta = () =>{
+        //Si se acaban las cartas en la baraja
+        if(baraja.length === 0){
+            throw 'No ahi cartas en la baraja'
+        }
+         
+        return baraja.pop();//Retorna y elimina el ultimo elemento de un array 
+    };
+
+
+
+    //Valor de la carta en sin utilizar ternarios
+
+    // const valorCarta = (carta) => {
+
+    //     let puntos = 0; //Valor en puntos de cada carta 
+
+    //     let valor = carta.substring(0,carta.length-1);//Necestio tomar el primer valor de la carta 2,3,4,5 o 10 entonces siempre va a ir desde la posicion 0 hasta la posicion final menos 1 para el 10 
+    //     console.log({valor});
+
+    //     if(isNaN(valor)){ //Mira si el valor es un numero  y devuelve un valor boleano 
+    //         //Entra si NO es un numero el valor 
+    //         if(valor == 'A'){
+    //             puntos = 11;
+    //             console.log(puntos)
+    //         }else{
+    //             puntos = 10;
+    //             console.log(puntos)
+    //         }
+
+
+    //     }else{
+    //         //Entra si ES un numero el valor 
+    //         puntos = parseInt(valor,10); //cambio el valor de tipo string a entero para poder hacer operaciones con el puntaje 
+    //         console.log({valor});
+    //         console.log(puntos + 5 );
+    //     }
+
+    // };
+
+
+
+    //Valor de la carta utilizando ternarios
+    const valorCarta2 = (carta) =>{
+        let valor = carta.substring(0,carta.length-1);//Necestio tomar el primer valor de la carta 2,3,4,5 o 10 entonces siempre va a ir desde la posicion 0 hasta la posicion final menos 1 para el 10 
+        
+        return (isNaN(valor))?//Entra si NO es un numero el valor 
+            (valor =='A')? 11:10//Si el valor es igual a A devuelve 11 si no devuelve 10
+            :parseInt(valor,10);//Entra si ES un numero el valor
+                                //cambio el valor de tipo string a entero 
     }
 
 
-    // console.log(baraja)//Arreglo sin barajar
-    baraja = _.shuffle (baraja); //Funcion que sirve para barajar apartir de la libreria Underscore.js
-    console.log(baraja);//Arreglo barajado
-    return baraja;
-};
 
-crearBaraja();
+    const acumularPuntos = () => {
+
+    };
+
+    //Turno de la computadora 
+    const turnoComputadora = (puntosMinimos)=>{
+        do {
+            const carta = tomarCarta();//Cuando se de click se toma una carta
+            puntosComputadora = puntosComputadora + valorCarta2(carta); //Se actualiza los puntos de la computadora 
+            actualizarPuntos[1].innerText = puntosComputadora;//Se toma el small en su posicion 1 que es el small de la computadora  y se coloca el puntaje de la computadora 
+            //Logica para la creacion de la imagen en el HTML 
+            const imagenCarta = document.createElement('img');//Crea un elemento img de HTML
+            imagenCarta.classList.add('carta');//Se le asgina la clase de las cartas
+            imagenCarta.src = `assets/cartas/${carta}.png`;//Se le asigna la imagen 
+            barajaComputadora.append(imagenCarta);//Se coloca al final del elemento
+
+            if(puntosMinimos > 21){//Si lo puntos del jugador son mayor a 21 entonces la computadora gana con cualquier carta 
+                break; //Se sale del ciclo 
+            }
+        } while ((puntosComputadora < puntosMinimos) && (puntosMinimos<=21));//tiene que ser menor a los puntos del jugador Y (&&) puntos debe ser menor o igual a 21  
 
 
-//Esta funcion me permite tomar una nueva carta 
-
-const tomarCarta = () =>{
-    //Si se acaban las cartas en la baraja
-    if(baraja.length === 0){
-        throw 'No ahi cartas en la baraja'
+        //Atento funcion de Javascript que me permite enviar este collback(Funcion que se envia como argumento) despues de un determindado tiemp
+        //En este caso 50 milsesimas de segundo
+        setTimeout(() => {
+            if(puntosComputadora === puntosMinimos){
+                alert('NADIE GANA :(');
+            }else if (puntosMinimos > 21) {
+                alert('COMPUTADORA GANA');
+            }else if (puntosComputadora > 21) {
+                alert('JUGADOR GANA');
+            } else {
+                alert('COMPUTADORA GANA');
+            }        
+        }, 50);//Modificar tiempo a su gusto
+        
     }
 
 
-    const carta = baraja.pop();//Elimina el ultimo elemento de un array 
-    // console.log(baraja);
-    // console.log(carta);
-    return carta;//retorna la carta  
-};
-
-
-//tomarCarta();
-
-//Valor de la carta en sin utilizar ternarios
-
-// const valorCarta = (carta) => {
-
-//     let puntos = 0; //Valor en puntos de cada carta 
-
-//     let valor = carta.substring(0,carta.length-1);//Necestio tomar el primer valor de la carta 2,3,4,5 o 10 entonces siempre va a ir desde la posicion 0 hasta la posicion final menos 1 para el 10 
-//     console.log({valor});
-
-//     if(isNaN(valor)){ //Mira si el valor es un numero  y devuelve un valor boleano 
-//         //Entra si NO es un numero el valor 
-//         if(valor == 'A'){
-//             puntos = 11;
-//             console.log(puntos)
-//         }else{
-//             puntos = 10;
-//             console.log(puntos)
-//         }
-
-
-//     }else{
-//         //Entra si ES un numero el valor 
-//         puntos = parseInt(valor,10); //cambio el valor de tipo string a entero para poder hacer operaciones con el puntaje 
-//         console.log({valor});
-//         console.log(puntos + 5 );
-//     }
-
-// };
 
 
 
+    //Eventos
+    //Evento de pedir
+    btnpedir.addEventListener('click', () => {
 
-
-
-//Valor de la carta utilizando ternarios
-const valorCarta2 = (carta) =>{
-    let valor = carta.substring(0,carta.length-1);//Necestio tomar el primer valor de la carta 2,3,4,5 o 10 entonces siempre va a ir desde la posicion 0 hasta la posicion final menos 1 para el 10 
-    
-    return (isNaN(valor))?//Entra si NO es un numero el valor 
-           (valor =='A')? 11:10//Si el valor es igual a A devuelve 11 si no devuelve 10
-           :parseInt(valor,10);//Entra si ES un numero el valor
-                              //cambio el valor de tipo string a entero 
-}
-
-
-
-//Turno de la computadora 
-
-const turnoComputadora = (puntosMinimos)=>{
-    do {
         const carta = tomarCarta();//Cuando se de click se toma una carta
-        puntosComputadora = puntosComputadora + valorCarta2(carta); //Se actualiza los puntos de la computadora 
-        actualizarPuntos[1].innerText = puntosComputadora;//Se toma el small en su posicion 1 que es el small de la computadora  y se coloca el puntaje de la computadora 
+
+        puntosJugador = puntosJugador + valorCarta2(carta); //Se actualiza los puntos del jugador 
+        actualizarPuntos[0].innerText = puntosJugador;//Se toma el small en su posicion 0 que es el small del jugador y se coloca el puntaje del jugador 
+        
+        
         //Logica para la creacion de la imagen en el HTML 
         const imagenCarta = document.createElement('img');//Crea un elemento img de HTML
         imagenCarta.classList.add('carta');//Se le asgina la clase de las cartas
         imagenCarta.src = `assets/cartas/${carta}.png`;//Se le asigna la imagen 
-        barajaComputadora.append(imagenCarta);//Se coloca al final del elemento
+        barajaJugador.append(imagenCarta);//Se coloca al final del elemento
 
-        if(puntosMinimos > 21){//Si lo puntos del jugador son mayor a 21 entonces la computadora gana con cualquier carta 
-            break; //Se sale del ciclo 
+
+        if(puntosJugador > 21){
+            console.error('PERDISTE');
+            btnpedir.disabled = true;//bloque el boton pedir
+            btndetener.disabled = true;//bloque el boton detener
+            turnoComputadora(puntosJugador);//Funcion para que juege la computadora 
+        }else if(puntosJugador === 21){
+            console.warn('LLEGASTE A 21 GENIAL !!!!!!1');
+            btnpedir.disabled = true;//bloque el boton pedir
+            btndetener.disabled = true;//bloque el boton detener
+            turnoComputadora(puntosJugador);//Funcion para que juege la computadora 
         }
-    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos<=21));//tiene que ser menor a los puntos del jugador Y (&&) puntos debe ser menor o igual a 21  
+        
+    })
 
 
-    //Atento funcion de Javascript que me permite enviar este collback(Funcion que se envia como argumento) despues de un determindado tiemp
-    //En este caso 50 milsesimas de segundo
-    setTimeout(() => {
-        if(puntosComputadora === puntosMinimos){
-            alert('NADIE GANA :(');
-        }else if (puntosMinimos > 21) {
-            alert('COMPUTADORA GANA');
-        }else if (puntosComputadora > 21) {
-            alert('JUGADOR GANA');
-        } else {
-            alert('COMPUTADORA GANA');
-        }        
-    }, 50);//Modificar tiempo a su gusto
-    
-}
-
-
-
-
-
-//Eventos
-//Evento de pedir
-btnpedir.addEventListener('click', () => {
-
-    const carta = tomarCarta();//Cuando se de click se toma una carta
-
-    puntosJugador = puntosJugador + valorCarta2(carta); //Se actualiza los puntos del jugador 
-    actualizarPuntos[0].innerText = puntosJugador;//Se toma el small en su posicion 0 que es el small del jugador y se coloca el puntaje del jugador 
-    
-    
-    //Logica para la creacion de la imagen en el HTML 
-    const imagenCarta = document.createElement('img');//Crea un elemento img de HTML
-    imagenCarta.classList.add('carta');//Se le asgina la clase de las cartas
-    imagenCarta.src = `assets/cartas/${carta}.png`;//Se le asigna la imagen 
-    barajaJugador.append(imagenCarta);//Se coloca al final del elemento
-
-
-    if(puntosJugador > 21){
-        console.error('PERDISTE');
+    //Evento de detener
+    btndetener.addEventListener('click', ()=> {
         btnpedir.disabled = true;//bloque el boton pedir
         btndetener.disabled = true;//bloque el boton detener
-        turnoComputadora(puntosJugador);//Funcion para que juege la computadora 
-    }else if(puntosJugador === 21){
-        console.warn('LLEGASTE A 21 GENIAL !!!!!!1');
-        btnpedir.disabled = true;//bloque el boton pedir
-        btndetener.disabled = true;//bloque el boton detener
-        turnoComputadora(puntosJugador);//Funcion para que juege la computadora 
-    }
+        turnoComputadora (puntosJugador);//Funcion para que juege la computadora 
+    })
+
+
+    //Evento nuevo juego 
+    btnuevo.addEventListener('click', ()=>{
+        btnpedir.disabled = false;//Desbloquea el boton pedir
+        btndetener.disabled = false;//Desbloquea el boton detener
+        actualizarPuntos[0].innerText = 0;//Resetea el valor
+        actualizarPuntos[1].innerText = 0;//Resetea el valor
+        crearJuego();
+        barajaJugador.innerHTML = '';//Quita las cartas del jugador
+        barajaComputadora.innerHTML = '';//Quita las cartas de la computadora
+    })
+
     
-})
+} )();
 
-
-//Evento de detener
-btndetener.addEventListener('click', ()=> {
-    btnpedir.disabled = true;//bloque el boton pedir
-    btndetener.disabled = true;//bloque el boton detener
-    turnoComputadora (puntosJugador);//Funcion para que juege la computadora 
-})
-
-
-//Evento nuevo juego 
-btnuevo.addEventListener('click', ()=>{
-    btnpedir.disabled = false;//Desbloquea el boton pedir
-    btndetener.disabled = false;//Desbloquea el boton detener
-    actualizarPuntos[0].innerText = 0;//Resetea el valor
-    actualizarPuntos[1].innerText = 0;//Resetea el valor
-    puntosJugador     = 0;//Resetea el valor
-    puntosComputadora = 0 ;//Resetea el valor
-    baraja            = [];//Resetea la baraja
-    crearBaraja();//Crea nuevamente la baraja
-    divCartasJugador.innerHTML = '';//Quita las cartas del jugador
-    divCartasComputadora.innerHTML = '';//Quita las cartas de la computadora
-})
 
 
 
