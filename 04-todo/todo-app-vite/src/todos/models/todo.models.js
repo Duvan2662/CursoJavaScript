711832da61b28f80d8fe3c971e02817e8b64e3e2
Duvan <duvanriano2626@gmail.com>
2024-03-18 11:43:04 -0500

    Descarga e implementacion de la librerioa uuid que sirve para identificar de manera unica nuestras notas

--- 04-todo/todo-app-vite/src/todos/models/todo.models.js
+++ 04-todo/todo-app-vite/src/todos/models/todo.models.js
@@ -1,7 +1,7 @@
-
+import { v4 as aleatorio } from "uuid"; //Libreria para generar id aleatorio
 export class Todo {
     constructor(descripccion){
-        this.id = 1;
+        this.id = aleatorio();
         this.descripccion = descripccion;
         this.estado = false;
         this.crearFecha = new Date();
