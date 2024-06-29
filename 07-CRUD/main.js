e2581ac0da58959671703ed769be86411eb7c535
Duvan <duvanriano2626@gmail.com>
2024-06-28 21:14:54 -0500

    CRUD terminado

--- 07-CRUD/main.js
+++ 07-CRUD/main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { UsersApp } from './src/usuarios/usuarios-app'
+import './style.css';
+import javascriptLogo from './javascript.svg';
+// import { BreakingbadApp } from './src/breakingbad/breakingbad-app';
+import { UsersApp } from './src/users/users-app';
 
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <h1 id="app-title">Hello Vite!</h1>
     <div class="card">
       
     </div>
     
   </div>
-`
-const element = document.querySelector('.card')
+`;
 
-UsersApp(element);
+const element = document.querySelector('.card');
 
+UsersApp( element );
